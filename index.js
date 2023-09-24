d746dfe5a326abe966ae44ac02904d05c55156b1
Nischal Shahi <nischal@keepitcleaner.com.au>
2023-09-25 01:13:58 +0545

    patch fixed

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CrossFadeImages = void 0;
+const CrossFadeImages_1 = require("./src/CrossFadeImages");
+Object.defineProperty(exports, "CrossFadeImages", { enumerable: true, get: function () { return CrossFadeImages_1.CrossFadeImages; } });
